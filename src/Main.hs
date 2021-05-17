6a1c4852ab0c1ea64550240a35f111558e0d17e6
Rohan Gautam <rohan1gautam@gmail.com>
2021-05-17 06:42:07 +0000

    working sleep example in rust!

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,7 @@ foreign import ccall "load_something" loadSomething :: CInt -> Bool
 
 main = do
   putStrLn "starting"
-  let loadop = loadSomething 40
+  let loadop = loadSomething 1400
   putStrLn $ show 4 ++ " * 2 = " ++ show loadop
   let input = 4
   let output = doubleInput input
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 // use std::thread;
-// use std::time::Duration;
+use std::time::Duration;
 
 #[no_mangle]
-pub extern "C" fn load_something(millis: i32) -> bool {
+pub extern "C" fn load_something(millis: u64) -> bool {
     println!("In rust, got: {}", millis);
+    std::thread::sleep(Duration::from_millis(millis));
     true
 }
 
