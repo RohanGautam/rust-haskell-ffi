3861abdc83a51bd79e78c21c7c1fb64d8acdce84
Rohan Gautam <rohan1gautam@gmail.com>
2021-05-17 06:40:09 +0000

    make rust compile to a  dynamic library

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,10 @@
-use std::thread;
-use std::time::Duration;
+// use std::thread;
+// use std::time::Duration;
 
 #[no_mangle]
 pub extern "C" fn load_something(millis: i32) -> bool {
-    sleep(millis);
+    println!("In rust, got: {}", millis);
     true
-}
-fn sleep(millis: i32) {
-    thread::sleep(Duration::from_millis(millis as u64));
 }
 
 #[no_mangle]
